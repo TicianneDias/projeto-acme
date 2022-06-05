a011db87fdc12f1d4173360b15379f00bab95c79
Ticianne <ticidias@gmail.com>
2022-06-05 11:26:15 -0300

    Feat: Criação e ajustes do NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Delivery } from './styles'
+import { Delivery, Nav, SearchBar, SearchContainer } from './styles'
+import { FiShoppingCart } from 'react-icons/fi'
+import { BiSearchAlt } from 'react-icons/bi'
 
 const NavBar = () => {
   return (
@@ -7,6 +9,21 @@ const NavBar = () => {
         <Delivery>
             <p>Frete grátis para compras acima de R$150</p>
         </Delivery>
+        <Nav>
+          <h1>ACME</h1>
+          <ul>
+              <li><a href="">Produtos</a></li>
+              <li><a href="">Favoritos</a></li>
+              <li><a href="">Carrinho</a></li>
+          </ul>
+          <SearchContainer>
+            <BiSearchAlt id='searchIcon'/>
+            <SearchBar
+                placeholder='Pesquisar Produto'>
+            </SearchBar>
+          </SearchContainer>
+          <a href=""><FiShoppingCart/></a>
+        </Nav>
     </>
   )
 }
--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -3,10 +3,14 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Overpass:wght@400;500&display=swap');
 //font-family: 'Overpass', sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap');
+//font-family: 'PT Sans', sans-serif;
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'PT Sans', sans-serif;
     }
 
     :root {
